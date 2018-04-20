dcad6fb67cca98636e272e65885e31bb5981ac2c
natasha miller <natasha.miller@rocketweb.com>
2018-04-20 14:39:43 +0300

    Google Address search validation on Checkout

--- view/frontend/web/js/view/shipping-mixin.js
+++ view/frontend/web/js/view/shipping-mixin.js
@@ -51,12 +51,22 @@ define(
         var mixin = {
             setShippingInformation: function () {
                 if (!this.validateShippingInformation()) {
-                    var errorElement = $("main #rw-google-complete-form div[class*='mage-error']");
+                    var errorElement = $("#rw-google-complete-form div[class*='mage-error'], #rw-google-complete-form div[class*='field-error']");
                     if(errorElement.length) {
                         $('#rw-google-complete-form').addClass('complete').show();
                     }
                 }
                 this._super();
+            },
+            saveNewAddress: function () {
+                this._super();
+
+                if (this.source.get('params.invalid')) {
+                    var errorElement = $(".modal-popup._show #rw-google-complete-form div[class*='field-error'], .modal-popup._show #rw-google-complete-form div[class*='mage-error']");
+                    if(errorElement.length) {
+                        $('#rw-google-complete-form').addClass('complete').show();
+                    }
+                }
             }
         };
 
